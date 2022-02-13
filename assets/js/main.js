8b11145b58096bf994625d48a49120f4a11d2c12
4z4quality <98920805+TheShovelGaming@users.noreply.github.com>
2022-02-13 22:57:44 +0300

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
-
+/**
+* Template Name: BizPage - v5.8.0
+* Template URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
 (function() {
   "use strict";
 
@@ -282,42 +287,6 @@
       }
     }
   });
-
-/**
-   * Panels Slider
-   */
- new Swiper('.panels-slider', {
-  speed: 400,
-  loop: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false
-  },
-  slidesPerView: 'auto',
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    },
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 20
-    },
-    640: {
-      slidesPerView: 4,
-      spaceBetween: 20
-    },
-    992: {
-      slidesPerView: 6,
-      spaceBetween: 20
-    }
-  }
-});
 
   /**
    * Testimonials slider
