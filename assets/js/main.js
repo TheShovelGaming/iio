3380b22fc03ae97b4ac8fbc0f764fdee7dc91b8f
4z4quality <98920805+TheShovelGaming@users.noreply.github.com>
2022-02-13 21:28:45 +0300

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -283,6 +283,42 @@
     }
   });
 
+/**
+   * Panels Slider
+   */
+ new Swiper('.panels-slider', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    },
+    992: {
+      slidesPerView: 6,
+      spaceBetween: 20
+    }
+  }
+});
+
   /**
    * Testimonials slider
    */
